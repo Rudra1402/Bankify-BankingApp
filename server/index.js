73b60e194072153072907dd9cdff0cc6b6863d20
Rudra Patel <rudra.190410116095@gmail.com>
2023-12-09 15:24:07 -0500

    firebase obj update

--- server/index.js
+++ server/index.js
@@ -6,7 +6,20 @@ const mongoose = require('mongoose')
 const fileUpload = require('express-fileupload')
 
 const admin = require('firebase-admin')
-const serviceAccountFirebase = require('./banking-app-b30cb-firebase-adminsdk-mm73m-1967c5c1f4.json')
+// const serviceAccountFirebase = require('./banking-app-b30cb-firebase-adminsdk-mm73m-1967c5c1f4.json')
+const serviceAccountFirebase = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 const app = express()
 const PORT = process.env.PORT || 8000
