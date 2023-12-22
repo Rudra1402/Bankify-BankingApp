a175e7b324ad1a30e310e2b01746a6e1313761fd
Rudra Patel <rudra.190410116095@gmail.com>
2023-12-22 15:39:47 -0500

    updated serving defined routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import AppProvider from './context/AppProvider'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
