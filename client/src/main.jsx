468820041caf548b3d864d786ddbf131df6a6664
Rudra Patel <rudra.190410116095@gmail.com>
2023-12-22 15:55:31 -0500

    serving routes issue, reverting back

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import AppProvider from './context/AppProvider'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
 )
